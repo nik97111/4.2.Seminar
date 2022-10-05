0c1429a6dd8a1a400eee979ee0ecbb1355015b21
nik <nik@MacBook-Air-nik.local>
2022-10-05 20:22:57 +0500

    решение задачи

---
+++ task_10.d.z./Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число и на выходе показывает 
+// вторую цифру этого числа.
+//456 -> 5, 782 -> 8, 918 -> 1 
+
+Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+
+int digitFirst = number / 10;
+int digitSecond = digitFirst / 10;
+int digitThird = digitSecond * 10;
+int digitFourth = digitFirst - digitThird;
+Console.WriteLine(digitFourth);
