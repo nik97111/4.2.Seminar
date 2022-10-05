c3701df69fd4cea0364d933cd93b41c320ff9d51
nik <nik@MacBook-Air-nik.local>
2022-10-06 00:10:16 +0500

    решение задачи

--- task_13.d.z./Program.cs
+++ task_13.d.z./Program.cs
@@ -2,6 +2,16 @@
 // что третьей цифры нет.
 // 645 -> 5, 78 -> третьей цифры нет, 32679 -> 6
 
-Console.Write("Введите число: ");
+Console.Write("Введите число не менее двухзначного: ");
 int number = int.Parse(Console.ReadLine());
 
+int digitThree = 0;
+
+if ((number >= 100) && (number < 1000)) 
+{
+    digitThree = number % 10;
+    Console.WriteLine(digitThree);
+} else 
+{
+    Console.WriteLine("Третьей цифры нет");
+}
