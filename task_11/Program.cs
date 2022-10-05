754542f9ba28b8f96b00a33b991b614626718366
nik <nik@MacBook-Air-nik.local>
2022-10-05 12:48:21 +0500

    решение задачи

---
+++ task_11/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру 
+// этого числа.
+// 456 -> 46, 782 -> 12, 918 -> 98
+
+int numberRandom  = new Random().Next(100, 1000);
+Console.WriteLine(numberRandom + " - рандомное число");
+
+int digitOne = numberRandom / 100;
+int digitTwo = numberRandom % 10;
+Console.WriteLine($"Трехзначное число {numberRandom} превратилось в двухзначное {digitOne}{digitTwo}");
+
