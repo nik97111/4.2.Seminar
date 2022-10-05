6fa3c75d987c2d235d2e510a4f8cb31f84837560
nik <nik@MacBook-Air-nik.local>
2022-10-05 22:20:19 +0500

    решение задачи

--- task_15.d.z./Program.cs
+++ task_15.d.z./Program.cs
@@ -1,3 +1,30 @@
 ﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели и 
 // проверяет, является ли этот день выходным.
 // 6 -> да, 7 -> да, 1 -> нет
+
+
+// 1-й способ
+
+Console.Write("Введите цифру от 1 до 7: ");
+int day = int.Parse(Console.ReadLine());
+
+if (day == 1) {
+    Console.WriteLine("Рабочий день");
+} else if (day == 2) {
+    Console.WriteLine("Рабочий день");
+} else if (day == 3) {
+    Console.WriteLine("Рабочий день");
+} else if (day ==4) {
+    Console.WriteLine("Рабочий день");
+} else if (day == 5) {
+    Console.WriteLine("Рабочий день");
+} else if (day == 6) {
+    Console.WriteLine("Выходной день");
+} else if (day == 7) {
+    Console.WriteLine("Выходной день");
+}
+
+
+// string[] daysOfWeek = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", 
+// "Воскресенье" };
+// Console.WriteLine(daysOfWeek);
