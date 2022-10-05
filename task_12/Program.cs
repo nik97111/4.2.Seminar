e3984c586c66e7b1fb11b73a445c6e131b0a0379
nik <nik@MacBook-Air-nik.local>
2022-10-05 17:44:01 +0500

    решение задачи

---
+++ task_12/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, 
+// является ли второе число кратным.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно 
+
+Console.Write("Введите первое число: ");
+int numberOne = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberTwo = int.Parse(Console.ReadLine());
+
+int a = numberTwo % 2;
+//Console.WriteLine(a);
+
+if ((numberTwo % 2) == 0) {
+    Console.WriteLine($"Второе число -> {numberTwo} является кратным");
+} else {
+    Console.WriteLine($"Второе число -> {numberTwo} не является кратным, остаток от деления -> {a}");
+}
+
+
+
