52527f5c8656b014aa09961ab75efd79c906ea43
nik <nik@MacBook-Air-nik.local>
2022-10-05 12:26:25 +0500

    дополнили решение

--- task_9/Program.cs
+++ task_9/Program.cs
@@ -5,7 +5,7 @@
 // 85 -> 8
 
 
-int numberRandom = new Random().Next (10, 99);
+int numberRandom = new Random().Next(10, 100);
 Console.WriteLine(numberRandom + " - рандомное число");
 
 int numberConsole = numberRandom;
@@ -25,3 +25,16 @@ if (digitOne > digitTwo) {
 } else {
     Console.WriteLine("Цифры числа равны");
 }
+
+
+// int num = new Random().Next(10, 100);
+// int a1 = num / 10;
+// int a2 = num % 10;
+// int max = a1;
+// Console.WriteLine($"{a1}, {a2}");
+
+// if (max < a2)
+// {
+//     max = a2;
+// }
+// Console.WriteLine($"Максимальная цифра в числе {num} -> {max}");
