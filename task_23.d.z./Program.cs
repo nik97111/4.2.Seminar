70161c880c2f7f62f4fcec1da6f1256d5f4a378d
nik <nik@MacBook-Air-nik.home>
2022-10-11 21:57:44 +0300

    решение задачи

---
+++ task_23.d.z./Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдает 
+// таблицу кубов чисел от 1 до (N).
+// 3 - 1, 8, 27
+// 5 - 1, 8, 27, 64, 125
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= number; i++)
+{
+    Console.WriteLine(i * i * i);
+}
