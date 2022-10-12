597661e2c46f4573486b8acbdbf0006fdecc93f5
nik <nik@MacBook-Air-nik.home>
2022-10-12 16:09:43 +0300

    решение задачи

---
+++ task_26/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число и выдает количество 
+// цифр в числе.
+// 456 -> 3, 78 -> 2, 89126 -> 5
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int count = 0;
+if (count < num)
+{
+    count = num % 10;
+}
+Console.WriteLine(count);
