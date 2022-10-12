dd69f39b3d43fb810b6d4acf8f6fbbe5c26932d2
nik <nik@MacBook-Air-nik.home>
2022-10-12 14:55:46 +0300

    решение задачи

--- task_24/Program.cs
+++ task_24/Program.cs
@@ -2,12 +2,35 @@
 // сумму чисел от 1 до А.
 // 7 - 28, 4- 10, 8 - 36
 
-Console. Write("Введите число: ");
-int number = int.Parse(Console.ReadLine());
+// Console. Write("Введите число: ");
+// int number = int.Parse(Console.ReadLine());
 
-int sum = 0;
-for (int i = 1; i <= number; i++)
+// int sum = 0;
+// for (int i = 1; i <= number; i++)
+// {
+//     sum = sum + i;
+// }
+// Console.WriteLine(sum);
+
+
+
+Console. Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма цифр = {GetSumNums(num)}");
+// int num1 = int.Parse(Console.ReadLine());
+// Console.WriteLine($"Сумма цифр = {GetSumNums(num1)}");
+// int num2 = int.Parse(Console.ReadLine());
+//   или    int result = GetSumNums(num2);
+// Console.WriteLine($"Сумма цифр = {result}");
+
+
+int GetSumNums(int number) 
 {
-    sum = sum + i;
+    int sum = 0;
+    while (number > 0)
+    {
+        sum += number;
+        number--;
+    }
+    return sum;
 }
-Console.WriteLine(sum);
