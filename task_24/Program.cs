b291878954f026129133284d383a2f27a32a17e5
nik <nik@MacBook-Air-nik.home>
2022-10-12 09:17:00 +0300

    решение задачи

---
+++ task_24/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдает
+// сумму чисел от 1 до А.
+// 7 - 28, 4- 10, 8 - 36
+
+Console. Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int sum = 0;
+for (int i = 1; i <= number; i++)
+{
+    sum = sum + i;
+}
+Console.WriteLine(sum);
