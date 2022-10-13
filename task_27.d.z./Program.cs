318be5fd2e098e712b555f1e0e457b631b6cb440
nik <nik@MacBook-Air-nik.home>
2022-10-13 18:59:08 +0300

    решение задачи

---
+++ task_27.d.z./Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая принимает на вход число и выдает сумму 
+// цифр в числе.
+// 452 -> 11, 82 -> 10, 9012 -> 12
+
+// Console.Write("Введите число: ");
+// int number = int.Parse(Console.ReadLine());
+
+// int sum = 0;
+// while (number > 0)
+// {
+//     int digit = number % 10;
+//     sum += digit;
+//     number /= 10;
+// }
+// Console.WriteLine(sum);
+
+
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма цифр числа = {SumDigitsNumber(num)}");
+
+int SumDigitsNumber(int number)
+{
+int sum = 0;
+while (number > 0)
+    {
+    int digit = number % 10;
+    sum += digit;
+    number /= 10;
+    }
+    Console.WriteLine(sum);
+    return sum;
+}
+
+
