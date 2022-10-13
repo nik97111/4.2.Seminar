caf1e3032b54518b5aa7c3423b1063fc0882e6d9
nik <nik@MacBook-Air-nik.local>
2022-10-13 09:34:28 +0300

    решение задачи

--- task_28/Program.cs
+++ task_28/Program.cs
@@ -2,25 +2,54 @@
 // произведение чисел от 1 до N.
 // 4 -> 24, 5 -> 120
 
-Console.Write("Введите число: ");
-int num = int.Parse(Console.ReadLine());
 
-//int sum = 1;
+
+// Console.Write("Введите число: ");
+// int num = int.Parse( Console.ReadLine());
+
+// int sum = 1;
 // for (int i = 1; i <= num; i++)
 // {
 //     sum = sum * i;
 // }
 // Console.WriteLine(sum);
 
+// int sum = 1;
+// int i = 1;
+// while(i < num)
+// {
+//     i++;
+//     sum *=i;
+// }
+// Console.WriteLine(sum);
 
-int sum = 1;
-int i = 0;
-while (i < num)
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Произведение чисел равно = {ProductNumbers(num)}");
+
+// int ProductNumbers(int number)
+// {
+//     int sum = 1;
+//     for (int i = 1; i <= number; i++)
+//     {
+//         sum *=i;
+//     }
+//     return sum;
+//     Console.WriteLine(sum);
+// }
+
+int ProductNumbers(int number)
 {
-    i++;
-    sum = sum * i;
+    int sum = 1;
+    int i = 1;
+    while (i < number)
+    {
+        i++;
+        sum *= i;
+    }
+    return sum;
+    Console.WriteLine(sum);
 }
-Console.WriteLine(sum);
-
 
 
