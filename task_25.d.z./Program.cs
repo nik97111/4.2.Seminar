5fe125e3259d24aa959e55d2aa403882788dd349
nik <nik@MacBook-Air-nik.home>
2022-10-13 22:23:08 +0300

    решение задачи

--- task_25.d.z./Program.cs
+++ task_25.d.z./Program.cs
@@ -3,6 +3,22 @@
 // 3, 5 -> 243 (3 в 5-й степени),  2, 4 -> 16 (2 в 4-й степени)
 
 
+Console.Write("Введите число A: ");
+int numA = int.Parse(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numB = int.Parse(Console.ReadLine());
+
+int order = 1;
+int i = 0;
+while (i < numB)
+{
+    order *= numA;
+    i++;
+}
+Console.WriteLine(order);
+
+
+
 
 // Console.Write("Введите число A: ");
 // int numA = int.Parse(Console.ReadLine());
@@ -15,17 +31,17 @@
 
 
 
-Console.Write("Введите число A: ");
-int numA = int.Parse(Console.ReadLine());
-Console.Write("Введите число B: ");
-int numB = int.Parse(Console.ReadLine());
+// Console.Write("Введите число A: ");
+// int numA = int.Parse(Console.ReadLine());
+// Console.Write("Введите число B: ");
+// int numB = int.Parse(Console.ReadLine());
 
-int order = numB;
-if (numA > 0 && numB > 0)
-{
-    Console.WriteLine($"Число А в степени числа B = " + Math.Pow(numA, order));
-} 
-else 
-{
-    Console.WriteLine("Введите число больше 0 ");
-}
+// int order = numB;
+// if (numA > 0 && numB > 0)
+// {
+//     Console.WriteLine($"Число А в степени числа B = " + Math.Pow(numA, order));
+// } 
+// else 
+// {
+//     Console.WriteLine("Введите число больше 0 ");
+// }
