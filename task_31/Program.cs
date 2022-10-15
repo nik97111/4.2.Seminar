b4acbfbc802e9e83909b396b0a6c58511903f280
nik <nik@MacBook-Air-nik.home>
2022-10-15 17:29:15 +0300

    решение задачи

--- task_31/Program.cs
+++ task_31/Program.cs
@@ -51,26 +51,47 @@
 
 
 int[] arrayRandom = new int[12];
-//int[] arrayRandom = int.Parse(Console.ReadLine());
 ArrayRandom(arrayRandom);
 
 int[] ArrayRandom(int[] arrayRandom) 
 {
-    int sumPositiveNumbers = 0;
-    int sumNegativeNumbers = 0;
+     int sumPositiveNumbers = 0;
+     int sumNegativeNumbers = 0;
 
-    for (int i = 0; i < arrayRandom.Length; i++)
-    {
-        arrayRandom[i] = new Random().Next(-9, 9);
+     for (int i = 0; i < arrayRandom.Length; i++)
+     {
+         arrayRandom[i] = new Random().Next(-9, 9);
 
-        if (arrayRandom[i] > 0)
-        {
-            sumPositiveNumbers += arrayRandom[i];
-        } else if (arrayRandom[i] < 0)
-        {
-            sumNegativeNumbers += arrayRandom[i];
-        }
-    }
-    Console.WriteLine($"Сумма положительных чисел = {sumPositiveNumbers}, сумма отрицательных чисел = {sumNegativeNumbers}");
-    return arrayRandom;
-}
+         if (arrayRandom[i] > 0)
+             sumPositiveNumbers += arrayRandom[i];
+           else if (arrayRandom[i] < 0)
+             sumNegativeNumbers += arrayRandom[i];
+     }
+     Console.WriteLine($"Сумма положительных чисел = {sumPositiveNumbers}, сумма отрицательных чисел = {sumNegativeNumbers}");
+     return arrayRandom;
+ }
+
+
+// Решение из лекции
+// int[] GetArray(int size, int minValue, int maxValue)
+// {
+//     int[] res = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         res[i] = new Random().Next(minValue, maxValue + 1);
+//         Console.Write($"{res[i]} ");
+//     }
+//     return res;
+// }
+// int[] array = GetArray(12, -9, 9);
+// int positiveSum = 0;
+// int negativeSum = 0;
+// foreach(int el in array)
+// {
+//     if (el > 0)
+//         positiveSum += el;
+//     else
+//         negativeSum += el;
+// }
+// Console.WriteLine();
+// Console.WriteLine($"Сумма положительных чисел = {positiveSum}, сумма отрицательных чисел = {negativeSum}");
