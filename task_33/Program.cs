79d35649e44cdd29d186222badb27b8d52d2a0a5
nik <nik@MacBook-Air-nik.home>
2022-10-16 23:39:48 +0300

    решение задачи

---
+++ task_33/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задайте массив. Напишите программу, которая определяет, присутсвует 
+// ли заданное число в массиве.
+// 4;  массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+
+
+// int[] array = new int[5];
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = new Random().Next(-9, 9);
+//     Console.Write($"{array[i]} ");
+// }
+//     Console.WriteLine();
+//     Console.Write("Введите число: ");
+//     int number = int.Parse(Console.ReadLine());
+    
+//     bool found = false;
+// for (int i = 0; i < array.Length; i++)
+//     {
+//         if (number == array[i]) {
+//             found = true;
+//             break;
+//         }
+//     }
+
+//     if (found) {
+//         Console.WriteLine("Есть число");
+//     } else {
+//         Console.WriteLine("Этого числа нет в массиве");
+//     }
+
+
+int[] array = new int[5];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-9, 9);
+    Console.Write($"{array[i]} ");
+}
+    Console.WriteLine();
+    Console.Write("Введите число: ");
+    int number = int.Parse(Console.ReadLine());
+    
+for (int i = 0; i < array.Length; i++)
+    {
+        if (number == array[i]) 
+        {   
+            Console.WriteLine("Это число есть в массиве");
+        } else 
+        {
+            Console.WriteLine("Этого числа нет в массиве");
+        }
+    }
