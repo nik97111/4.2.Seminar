88f64e3a2f8e6392e6edfd933e61535f07a59746
nik <nik@MacBook-Air-nik.home>
2022-10-16 16:19:22 +0300

    решение задачи

--- task_32/Program.cs
+++ task_32/Program.cs
@@ -3,18 +3,39 @@
 // [-4, -8, 8, 2] -> [4, 8, -8, -2]
 
 
-int[] array = new int[4];
+// int[] array = new int[4];
 
-for (int i = 0; i < array.Length; i++)
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = new Random().Next(-9, 9);
+//     Console.Write($"{array[i]} ");
+//     array[i] = -array[i];
+// }
+// Console.WriteLine();
+// for (int i = 0; i < array.Length; i++)
+// {
+//     Console.Write($"{array[i]} ");
+// }
+
+
+
+int[] array = new int[4];
+Array(array);
+
+int[] Array(int[] array)
 {
-    array[i] = new Random().Next(-9, 9);
-    Console.Write($"{array[i]} ");
-    array[i] = -array[i];
-}
-Console.WriteLine();
-for (int i = 0; i < array.Length; i++)
-{
-    Console.Write($"{array[i]} ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-9, 9);
+        Console.Write($"{array[i]} ");
+        array[i] = -array[i];
+    }
+    Console.WriteLine();
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    return array;
 }
 
 
