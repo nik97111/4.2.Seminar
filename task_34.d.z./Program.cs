fd886fbf9524fceae371669171cbc6c3b2fdd47b
nik <nik@MacBook-Air-nik.home>
2022-10-17 19:47:22 +0300

    Решение задачи

---
+++ task_34.d.z./Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте массив, заполненный случайными положительными трехзначными 
+// числами. Напишите программу, которая покажет количество четных чисел 
+// в массиве
+// [345, 987, 568, 234] -> 2
+
+
+// int[] arrayRandom = new int[4];
+// int countPositiveNumbers = 0;
+
+// for (int i = 0; i < arrayRandom.Length; i++)
+// {
+//     arrayRandom[i] = new Random().Next(1000);
+//     Console.Write($"{arrayRandom[i]} ");
+//     if ((arrayRandom[i]) % 2 == 0)
+//         countPositiveNumbers += 1;
+// }
+// Console.WriteLine();
+// Console.WriteLine($"Четных чисел в массиве - {countPositiveNumbers}");
+
+
+int[] arrayRandom = new int[4];
+int countPositiveNumbers = 0;
+ArrayRandom(arrayRandom);
+
+int[] ArrayRandom(int[] arrayRandom)
+{
+    for (int i = 0; i < arrayRandom.Length; i++)
+    {
+        arrayRandom[i] = new Random().Next(1000);
+        Console.Write($"{arrayRandom[i]} ");
+        if ((arrayRandom[i]) % 2 == 0)
+            countPositiveNumbers += 1;
+    }
+    return arrayRandom;
+}
+Console.WriteLine();
+Console.WriteLine($"Четных чисел в массиве - {countPositiveNumbers}");
