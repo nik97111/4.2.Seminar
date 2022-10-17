5693c2ff1004dc7137cec36896851c1b246eea0e
nik <nik@MacBook-Air-nik.home>
2022-10-17 19:18:54 +0300

    Решение задачи

--- task_37/Program.cs
+++ task_37/Program.cs
@@ -5,22 +5,6 @@
 // [6, 7, 3, 6] -> 36 21
 
 
-
-// int[] arr = {4, 6, 3, 7};
-// int sumFirstLast = 0;
-// int sumSecondPenultimate = 0;
-
-// sumFirstLast = arr.First() * arr.Last();
-// sumSecondPenultimate = ((arr.First() - 1) * (arr.Last() - 1));
-// int[] arrRes = new int[]{sumFirstLast, sumSecondPenultimate};
-// for (int i = 0; i < arrRes.Length; i++)
-// {
-//     Console.WriteLine();
-//     Console.Write($"{arr[i]} -> {arrRes[i]} ");
-// }
-
-
-
 int[] array = new int[] { 4, 6, 5, 19, 6, 3, 7 };
 
 var i = 0;
