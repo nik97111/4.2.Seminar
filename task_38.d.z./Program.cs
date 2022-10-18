53bfb9444c2651090597949d4dbf8c0519f4191d
nik <nik@MacBook-Air-nik.home>
2022-10-18 12:35:44 +0300

    Решение задачи

--- task_38.d.z./Program.cs
+++ task_38.d.z./Program.cs
@@ -3,6 +3,67 @@
 // [3, 7, 22, 2, 78] -> 76
 
 
+// int[] arrayRandom = new int[6];
+// for (int i = 0; i < arrayRandom.Length; i++)
+// {
+//     arrayRandom[i] = new Random().Next(1, 100);
+//     Console.Write($"{arrayRandom[i]} ");
+// }
+
+// int minEl = arrayRandom[0];
+// int maxEl = arrayRandom[0];
+// int difference = 0;
+
+// for (int i = 1; i < arrayRandom.Length; i++)
+// {
+//     if (arrayRandom[i] > maxEl)
+//     {
+//         maxEl = arrayRandom[i];
+//     } else if (arrayRandom[i] < minEl)
+//     {
+//         minEl = arrayRandom[i];
+//     }
+// }
+// difference = maxEl - minEl;
+// Console.WriteLine();
+// Console.WriteLine(difference);
+
+
+
+int[] arrayRandom = new int[6];
+ArrayRandom(arrayRandom);
+
+int[] ArrayRandom(int[] arrayRandom)
+{
+    for (int i = 0; i < arrayRandom.Length; i++)
+    {
+        arrayRandom[i] = new Random().Next(1, 100);
+        Console.Write($"{arrayRandom[i]} ");
+    }
+
+    int minEl = arrayRandom[0];
+    int maxEl = arrayRandom[0];
+    int difference = 0;
+
+    for (int i = 1; i < arrayRandom.Length; i++)
+    {
+        if (arrayRandom[i] > maxEl)
+        {
+            maxEl = arrayRandom[i];
+        } else if (arrayRandom[i] < minEl)
+        {
+            minEl = arrayRandom[i];
+        }
+
+    }
+    difference = maxEl - minEl;
+    Console.WriteLine();
+    Console.WriteLine(difference);
+
+    return arrayRandom;
+}
+
+
 
 // int[] arrayRandom = new int[6];
 // int difference = 0;
@@ -17,20 +78,20 @@
 //     Console.WriteLine($"Разница = {difference}");
 
 
-int[] arrayRandom = new int[6];
-int difference = 0;
-ArrayRandom(arrayRandom);
+// int[] arrayRandom = new int[6];
+// int difference = 0;
+// ArrayRandom(arrayRandom);
 
-int[] ArrayRandom(int[] arrayRandom)
-{
-for (int i = 0; i < arrayRandom.Length; i++)
-{
-    arrayRandom[i] = new Random().Next(1, 100);
-    Console.Write($"{arrayRandom[i]} ");
-}
-    difference = arrayRandom.Max() - arrayRandom.Min();
-    Console.WriteLine();
-    Console.WriteLine($"Разница = {difference}");
+// int[] ArrayRandom(int[] arrayRandom)
+// {
+// for (int i = 0; i < arrayRandom.Length; i++)
+// {
+//     arrayRandom[i] = new Random().Next(1, 100);
+//     Console.Write($"{arrayRandom[i]} ");
+// }
+//     difference = arrayRandom.Max() - arrayRandom.Min();
+//     Console.WriteLine();
+//     Console.WriteLine($"Разница = {difference}");
 
-    return arrayRandom;
-}
+//     return arrayRandom;
+// }
