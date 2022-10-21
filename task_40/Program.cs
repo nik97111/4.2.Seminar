40a16af58ce45aa2a5b6ed7e7c536e27bd55248e
nik <nik@MacBook-Air-nik.home>
2022-10-21 18:01:04 +0300

    решение задачи

--- task_40/Program.cs
+++ task_40/Program.cs
@@ -4,20 +4,55 @@
 // суммы двух других сторон. 
 
 
-Console.WriteLine("Введите первое число: ");
-int numFirst = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите второе число: ");
-int numSecond = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите третье число: ");
-int numThree = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите первое число: ");
+// int numFirst = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int numSecond = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите третье число: ");
+// int numThree = int.Parse(Console.ReadLine());
 
 
-if ((numFirst < (numSecond + numThree)) && (numSecond < (numFirst + numThree)) 
-&& (numThree < (numFirst + numSecond)))
-    Console.WriteLine("Треугольник может существовать");
-else 
-    Console.WriteLine("Треугольник не может существовать");
+// if ((numFirst < (numSecond + numThree)) && (numSecond < (numFirst + numThree)) 
+// && (numThree < (numFirst + numSecond)))
+//     Console.WriteLine("Треугольник может существовать");
+// else 
+//     Console.WriteLine("Треугольник не может существовать");
 
+
+
+int numberFirst = GetNumber();
+int numberSecond = GetNumber();
+int numberThree = GetNumber();
+PrintRes(numberFirst, numberSecond, numberThree);
+
+
+int GetNumber()
+{
+    Console.Write("Введите число: ");
+    int number = int.Parse(Console.ReadLine()); 
+    return number;
+}
+
+void PrintRes(int numFirst, int numSecond, int numThree)
+{
+    if (numFirst > 0 && numSecond > 0 && numThree > 0)
+    {
+        if ((numFirst < (numSecond + numThree)) && (numSecond < (numFirst + numThree)) 
+        && (numThree < (numFirst + numSecond)))
+        {   
+            Console.WriteLine("Треугольник может существовать");
+        }
+        else 
+        {
+            Console.WriteLine("Треугольник не может существовать");
+        }
+    }
+    else 
+    {
+        Console.WriteLine("Введите число больше нуля");
+    }
+    
+}
 
 
 
