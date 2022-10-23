59c499ffa0cb1a4146cdcdcb7793898335063462
nik <nik@MacBook-Air-nik.home>
2022-10-23 17:28:01 +0300

    решение задачи

---
+++ task_45/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая будет создавать копию заданного массива 
+// с помощью поэлементного копирования.
+
+
+
+int[] arrayFirst = {3, 9, 0, 6, 7};
+int[] arraySecond = new int[arrayFirst.Length];
+
+for (int i = 0; i < arrayFirst.Length; i++)
+{
+    arraySecond[i] = arrayFirst[i];
+    Console.Write(arraySecond[i] + " ");
+}
+
+
+
