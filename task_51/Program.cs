48c1951a7cdf81a3b70e4180404c94d90d8b8ced
nik <nik@MacBook-Air-nik.home>
2022-10-26 15:59:42 +0300

    решение задачи

--- task_51/Program.cs
+++ task_51/Program.cs
@@ -7,3 +7,55 @@
 // Сумма элементов главной диагонали: 1+9+2 = 12
 
 
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] array = FillArray(rows, columns, 0, 10);
+PrintArray(array);
+int result = GetArray(array, rows, columns);
+Console.Write(result);
+
+int[,] FillArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] arr = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return arr;
+}
+
+int GetArray(int[,] arr, int m, int n)
+{
+    int sumEls = 0;
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+            {
+                sumEls += arr[i, j];
+            }
+        }
+    }
+    return sumEls;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
