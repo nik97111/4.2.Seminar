87d09cb9c048e8a25030b3fc70249b64ce8f5d29
nik <nik@MacBook-Air-nik.home>
2022-10-27 21:56:46 +0300

    решение задачи

--- task_47.d.z./Program.cs
+++ task_47.d.z./Program.cs
@@ -43,27 +43,3 @@ void PrintArray(double[,] inArray)
         Console.WriteLine();
     }
 }
-
-
-// using System;
-// namespace CSharp_Shell
-// { public static class Program
-//     { public static void Main()
-//         { Int32 k, l, m, n;
-//             Console.Write("m n: ");
-//             var s = Console.ReadLine().Split(" ");
-//             m = Convert.ToInt32(s[0]);
-//             n = Convert.ToInt32(s[1]);
-//             Random rnd = new Random();
-//             double [,] A = new double [m,n];
-//             for (k = 0; k < m; k++)
-//             { 
-//                 for (l = 0; l < n; l++)
-//                 { A[k,l]=20.0*rnd.Next()/2147483648-10.0;
-//             Console.Write(String.Format("{0,5:f1}", A[k,l])); 
-//                 } 
-//             Console.WriteLine(); 
-//             } 
-//         } 
-//     } 
-//}
