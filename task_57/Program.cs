904a012515a06db5cb88c540ee73d69edbd74961
nik <nik@MacBook-Air-nik.home>
2022-11-10 23:17:28 +0300

    решение задачи

--- task_57/Program.cs
+++ task_57/Program.cs
@@ -17,3 +17,54 @@
 // 6 встречается 2 раза
 
 
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+PrintArrayEl(array);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue) 
+{
+    
+    int[,] arr = new int[m, n];
+    for (int i = 0; i < m; i++ ) 
+    {
+        for (int j = 0; j < n; j++) 
+        {
+            arr[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return arr;
+}
+
+void PrintArray(int[,] inArray) 
+{
+    for (int i = 0; i < inArray.GetLength(0); i++ ) 
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++) 
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void PrintArrayEl(int[,] inArray) 
+{
+    int[] arr = new int[10];
+    for (int i = 0; i < inArray.GetLength(0); i++ ) 
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++) 
+        {
+            arr[inArray[i, j]] += 1;
+        }
+    }
+
+    for (int i = 0; i < 10; i++) {
+        Console.WriteLine("Элемент " + i + " встречается " + arr[i]);
+    }
+}
+
