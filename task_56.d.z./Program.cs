3e6acef3d4d527980180b4085010aca134c09828
nik <nik@MacBook-Air-nik.local>
2022-11-20 11:26:48 +0300

    решение задачи

--- task_56.d.z./Program.cs
+++ task_56.d.z./Program.cs
@@ -1,4 +1,4 @@
-﻿// Задайте прямоугольный двумерный массив. Напишите программу, 
+﻿// Задайте прямоугольный двухмерный массив. Напишите программу, 
 // которая будет находить строку с наименьшей суммой элементов.
 // Например, задан массив:
 // 1 4 7 2
@@ -7,3 +7,42 @@
 // 5 2 6 7
 // Программа считает сумму элементов в каждой строке и выдаёт номер 
 // строки с наименьшей суммой элементов: 1-я строка
+
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine());
+
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+// PrintArrayEl(array);
+// Console.WriteLine();
+// PrintArray(array);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue) 
+{
+    
+    int[,] arr = new int[m, n];
+    for (int i = 0; i < m; i++ ) 
+    {
+        for (int j = 0; j < n; j++) 
+        {
+            arr[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return arr;
+}
+
+void PrintArray(int[,] inArray) 
+{
+    for (int i = 0; i < inArray.GetLength(0); i++ ) 
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++) 
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
