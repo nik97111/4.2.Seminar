79029dcf3bf0f1457b374ae769242ef62e81e9b1
nik <nik@MacBook-Air-nik.home>
2022-11-29 23:20:36 +0300

    решение задачи

--- task_66.d.z./Program.cs
+++ task_66.d.z./Program.cs
@@ -9,27 +9,16 @@ int M = int.Parse(Console.ReadLine());
 Console.Write("Введите число N: ");
 int N = int.Parse(Console.ReadLine());
 
-var sum = 0;
 
-if (M < N)
+int SumNumbers(int M, int N)
 {
-    for (int i = M; i <= N; i++)
+    if (M == N)
     {
-        Console.Write($" {i}");
-        sum += i;
+        return M;
     }
+
+    return M + SumNumbers(M + 1, N);
 }
-else
-{
-    for (int i = N; i <= M; i++)
-    {
-        Console.Write($" {i}");
-        sum += i;
-    }
-}
-Console.WriteLine();
-Console.WriteLine(sum);
 
-
-
-       
+int res = SumNumbers(M, N);
+Console.WriteLine(res);
